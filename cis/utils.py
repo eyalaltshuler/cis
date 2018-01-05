e5c8d9a8ca16fc6d88b091935c986df1f7349530
Eyal <eyal.altshuler@gmail.com>
2018-01-06 00:40:51 +0200

    dataset news

--- cis/utils.py
+++ cis/utils.py
@@ -167,3 +167,45 @@ def create_dataset_different_sizes(input_path, output_path, db_name):
     finally:
         sc.stop()
         print 'done'
+
+
+def create_dataset_different_sizes_news(input_path, output_path, db_name):
+    try:
+        sc = get_spark_context()
+        num_partitions = 40
+        rdd = sc.textFile(input_path)
+        rdd.cache()
+        print 'loaded rdd from %s' % input_path
+
+        xsmall_rdd = rdd.sample(False, 0.11)
+        xsmall_rdd = xsmall_rdd.repartition(num_partitions)
+        path = os.path.join(output_path, db_name + '-xsmall')
+        xsmall_rdd.saveAsTextFile(path)
+        print 'Created db at %s' % path
+
+        path = os.path.join(output_path, db_name + '-small')
+        small_rdd = rdd.sample(False, 0.22)
+        small_rdd = small_rdd.repartition(num_partitions)
+        small_rdd.saveAsTextFile(path)
+        print 'Created db at %s' % path
+
+        path = os.path.join(output_path, db_name + '-medium')
+        medium_rdd = rdd.sample(False, 0.33)
+        medium_rdd = medium_rdd.repartition(num_partitions)
+        medium_rdd.saveAsTextFile(path)
+        print 'Created db at %s' % path
+
+        path = os.path.join(output_path, db_name + '-large')
+        large_rdd = rdd.sample(False, 0.44)
+        large_rdd = large_rdd.repartition(num_partitions)
+        large_rdd.saveAsTextFile(path)
+        print 'Created db at %s' % path
+
+        path = os.path.join(output_path, db_name + '-xlarge')
+        xlarge_rdd = rdd.sample(False, 0.55)
+        xlarge_rdd = xlarge_rdd.repartition(num_partitions)
+        xlarge_rdd.saveAsTextFile(path)
+        print 'Created db at %s' % path
+    finally:
+        sc.stop()
+        print 'done'
